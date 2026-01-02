6ca67e5f2ae912c61e961482561e4c910b2cac96
Mohammad-Waris <122534235+Mohammad-Waris@users.noreply.github.com>
2026-01-02 23:09:55 +0530

    Learned about Stack and Heap

--- week1/test.js
+++ week1/test.js
@@ -1,17 +1,35 @@
 "use strict"; //for newer versions
-console.log("Hello World");
-let temporary="1234";
+// console.log("Hello World");
+// let temporary="1234";
 
 //checking type by typeof or typeof()
-console.log(typeof temporary);
+// console.log(typeof temporary);
 
 //type conversion by converting string into number
-temporary=Number(temporary)
-console.log(temporary);
-console.log(typeof temporary)
+// temporary=Number(temporary)
+// console.log(temporary);
+// console.log(typeof temporary)
 
 //Note that the 
 //only string like "123" can be converted to Number 
 // "123abc" results in NaN
 //"" also results in Nan
 //"true/false" can be converted to 1/0
+
+//== and === works differently 
+
+// ====================================================
+//Primitive uses stack whereas Non-Primitive uses heaps
+let userOne={
+    name:"Waris",
+    email:"waris123@gmail.com"
+}
+console.log("User One "+userOne.email);
+console.log("Copying userOne to userTwo")
+let userTwo=userOne;
+console.log("Printing userTwo email")
+console.log("User Two "+userTwo.email)
+console.log("Changing email of userTwo")
+userTwo.email="chandan123@gmail.com"
+console.log("Printing email of userOne")
+console.log("User One "+userOne.email);
