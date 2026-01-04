065866f9958bc4dd30cfb76d02e8794ccbcad458
Mohammad-Waris <122534235+Mohammad-Waris@users.noreply.github.com>
2026-01-04 10:33:00 +0530

    Learned about Arrays and especially Splice and slice methods

--- week1/test.js
+++ week1/test.js
@@ -42,4 +42,15 @@ const max=6;
 // console.log(Math.random())
 // console.log(Math.random()*(max-min+1))
 // console.log(Math.random()*(max-min+1)+1)
-console.log(Math.floor(Math.random()*(max-min+1)+1))
+// console.log(Math.floor(Math.random()*(max-min+1)+1))
+
+// =====================================================================
+const arr1=[9,8,7,6,5,4]
+const arr2=[9,8,7,6,5,4]
+console.log(arr1)
+console.log("Slice:"+arr1.slice(0,3))
+console.log(arr1)
+console.log("***********************************")
+console.log(arr2)
+console.log("Splice:"+arr2.splice(0,3))
+console.log(arr2)
