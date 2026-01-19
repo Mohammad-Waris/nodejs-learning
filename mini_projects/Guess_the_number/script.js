5516cf7bbcdd1f6152623f2e8d917b6de1c529d4
Mohammad-Waris <122534235+Mohammad-Waris@users.noreply.github.com>
2026-01-20 00:58:51 +0530

    created the global variables and defined half of the logic of it. WIll make the rest of the functions working tomorrow

---
+++ mini_projects/Guess_the_number/script.js
@@ -0,0 +1,60 @@
+const random = parseInt(Math.random() * 100 + 1);
+const submit = document.getElementById("subt");
+const userInput = document.getElementById("guessField");
+const guessSlot = document.querySelector(".guesses");
+const remaining = document.querySelector(".lastResult");
+const loworHi = document.querySelector(".lowOrHi");
+const startOver = document.querySelector(".resultParas");
+
+const p = document.createElement("p");
+
+let prevGuess = [];
+let numGuess = 1;
+
+let playGame = true;
+
+if (playGame) {
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    const guess = parseInt(userInput.value);
+    validateGuess(guess);
+  });
+}
+
+function validateGuess(guess) {
+  if (isNaN(guess)) alert("Please enter a valid number");
+  else if (guess < 1 || guess > 100) alert("Enter number withing the range");
+  else {
+    prevGuess.push(guess);
+    if (numGuess == 11) {
+      displayGuess(guess);
+      displayMessage(`Game over. Last Guess was ${random}`);
+      endGame();
+    } else {
+      displayGuess(guess);
+      checkGuess(guess);
+      numGuess++;
+    }
+  }
+}
+
+//for printing the validated guess
+function checkGuess(guess) {
+  if (guess === random) {
+    displayMessage(`You guessed it right`);
+    endGame()
+  } else if (guess < random) displayMessage(`Your guess ${guess} is less than the number`);
+  else{
+    displayMessage(`Your guess ${guess} is more than the number`)
+  }
+}
+
+//for displaying the message
+function displayMessage(message) {}
+
+//displaying the guess
+function displayGuess(guess) {}
+
+function endGame() {}
+
+function newGame() {}
